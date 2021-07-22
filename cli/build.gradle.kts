87a213d72a6c5d9625b0e195b5b2068c5ad282eb
Vladislav.Tankov <vdtankov@gmail.com>
2021-07-22 17:53:13 +0300

    Introduce release to the GitHub releases

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -1,9 +1,12 @@
+import com.github.breadmoirai.githubreleaseplugin.GithubReleaseTask
+import tanvd.kosogor.proxy.shadowJar
+
 group = rootProject.group
 version = rootProject.version
 
 
 plugins {
-    application apply true
+    id("com.github.breadmoirai.github-release") version "2.2.12" apply true
 }
 
 dependencies {
@@ -16,6 +19,30 @@ dependencies {
     implementation("ch.qos.logback", "logback-classic", "1.2.3")
 }
 
-application {
-    mainClass.set("io.paddle.ApplicationKt")
+val shadowJar = shadowJar {
+    jar {
+        mainClass = "io.paddle.ApplicationKt"
+    }
+}.apply {
+    task.archiveClassifier.set("")
+    task.archiveBaseName.set("paddle")
+
+    task.from(file("src/main/resources/version.txt").apply {
+        if (!exists()) {
+            parentFile.mkdirs()
+            createNewFile()
+        }
+
+        writeText(project.version.toString())
+    })
+}
+
+githubRelease {
+    token(System.getenv("GITHUB_TOKEN"))
+    owner("tanvd")
+    releaseAssets(shadowJar.task.archiveFile.get())
+}
+
+tasks.withType(GithubReleaseTask::class) {
+    dependsOn("shadowJar")
 }
