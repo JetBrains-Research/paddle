31375572f0fb7c755fa6a8f6bdef7cfe70b51d79
Sokolvyak Sergey <zhvkgj@yandex.ru>
2021-08-27 12:58:47 +0300

    Another review fixes

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -16,5 +16,5 @@ dependencies {
     implementation("com.charleskorn.kaml", "kaml", "0.34.0")
     implementation("org.jetbrains.kotlinx", "kotlinx-serialization-json", "1.2.1")
 
-    implementation("org.jetbrains.kotlin", "kotlin-reflect", "1.5.21")
+    implementation(kotlin("reflect"))
 }
