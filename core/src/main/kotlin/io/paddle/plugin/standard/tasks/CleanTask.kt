210745a2ea575ad5dc56fea52ba901e9bf2e16d3
Oleg Smirnov <sm-oleg1209@yandex.ru>
2021-09-29 11:34:01 +0300

    Fix CleanTask: now symlink targets are not deleted

--- core/src/main/kotlin/io/paddle/plugin/standard/tasks/CleanTask.kt
+++ core/src/main/kotlin/io/paddle/plugin/standard/tasks/CleanTask.kt
@@ -5,6 +5,7 @@ import io.paddle.project.Tasks
 import io.paddle.tasks.Task
 import io.paddle.utils.tasks.TaskDefaultGroups
 import java.io.File
+import java.nio.file.Files
 
 class CleanTask(project: Project) : Task(project) {
     var locations = ArrayList<File>()
@@ -19,10 +20,18 @@ class CleanTask(project: Project) : Task(project) {
 
     override fun act() {
         for (location in locations) {
-            location.deleteRecursively()
+            location.deleteRecursivelyWithoutSymlinks()
         }
     }
 }
 
 val Tasks.clean: CleanTask
     get() = this.getOrFail("clean") as CleanTask
+
+fun File.deleteRecursivelyWithoutSymlinks() {
+    if (this.isDirectory && !Files.isSymbolicLink(this.toPath())) {
+        val children = this.listFiles() ?: return
+        children.forEach { it.deleteRecursivelyWithoutSymlinks() }
+    }
+    this.delete()
+}
