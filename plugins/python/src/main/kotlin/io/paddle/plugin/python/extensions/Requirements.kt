bdd9fcd7aab64964829076a8aad65e74e76393d9
Oleg Smirnov <sm-oleg1209@yandex.ru>
2021-10-06 13:09:25 +0300

    Drop requirements file from schema

--- plugins/python/src/main/kotlin/io/paddle/plugin/python/extensions/Requirements.kt
+++ plugins/python/src/main/kotlin/io/paddle/plugin/python/extensions/Requirements.kt
@@ -5,24 +5,21 @@ import io.paddle.utils.Hashable
 import io.paddle.utils.config.ConfigurationView
 import io.paddle.utils.ext.Extendable
 import io.paddle.utils.hashable
-import java.io.File
 
 val Project.requirements: Requirements
     get() = extensions.get(Requirements.Extension.key)!!
 
-class Requirements(val descriptors: MutableList<Descriptor>, val files: MutableList<File>) : Hashable {
+class Requirements(val descriptors: MutableList<Descriptor>) : Hashable {
     object Extension : Project.Extension<Requirements> {
         override val key: Extendable.Key<Requirements> = Extendable.Key()
 
         override fun create(project: Project): Requirements {
             val config = object : ConfigurationView("requirements", project.config) {
-                val requirementsFile by string("file", default = "requirements.txt")
                 val libraries by list<Map<String, String>>("libraries", default = emptyList())
             }
+            val descriptors = config.libraries.map { Descriptor(it["name"]!!, it["version"]!!) }.toMutableList()
 
-            val libraries = config.libraries.map { Descriptor(it["name"]!!, it["version"]!!) }.toMutableList()
-
-            return Requirements(libraries, mutableListOf(File(project.workDir, config.requirementsFile)))
+            return Requirements(descriptors)
         }
     }
 
@@ -35,6 +32,6 @@ class Requirements(val descriptors: MutableList<Descriptor>, val files: MutableL
     }
 
     override fun hash(): String {
-        return (files.map { it.hashable() } + descriptors).hashable().hash()
+        return descriptors.hashable().hash()
     }
 }
