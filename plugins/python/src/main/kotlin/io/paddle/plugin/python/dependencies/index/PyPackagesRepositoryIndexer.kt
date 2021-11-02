9de0bdd3e8807c15e7a768142439a145cf6f3611
Oleg Smirnov <sm-oleg1209@yandex.ru>
2021-11-02 08:19:55 +0300

    Update handling exceptions for PyPackagesRepositoryIndexer.kt

--- plugins/python/src/main/kotlin/io/paddle/plugin/python/dependencies/index/PyPackagesRepositoryIndexer.kt
+++ plugins/python/src/main/kotlin/io/paddle/plugin/python/dependencies/index/PyPackagesRepositoryIndexer.kt
@@ -2,6 +2,7 @@ package io.paddle.plugin.python.dependencies.index
 
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import io.ktor.http.*
 import io.paddle.plugin.python.dependencies.index.distributions.PyDistributionInfo
 import io.paddle.plugin.python.dependencies.index.metadata.JsonPackageMetadataInfo
 import io.paddle.plugin.python.dependencies.index.utils.*
@@ -11,9 +12,13 @@ import org.jsoup.Jsoup
 
 object PyPackagesRepositoryIndexer {
     suspend fun downloadPackagesNames(repository: PyPackagesRepository): Collection<PyPackageName> {
-        return httpClient.request<HttpStatement>(repository.urlSimple).execute { response ->
-            val allNamesDocument = Jsoup.parse(response.readText())
-            return@execute allNamesDocument.body().getElementsByTag("a").map { it.text() }
+        return try {
+            httpClient.request<HttpStatement>(repository.urlSimple).execute { response ->
+                val allNamesDocument = Jsoup.parse(response.readText())
+                return@execute allNamesDocument.body().getElementsByTag("a").map { it.text() }
+            }
+        } catch (exception: Throwable) {
+            error("Failed to update index of available packages for PyPI repository ${repository.name}: ${repository.url}")
         }
     }
 
@@ -28,13 +33,17 @@ object PyPackagesRepositoryIndexer {
                     .mapNotNull { PyDistributionInfo.fromString(it.text()) }
             }
         } catch (exception: Throwable) {
-            emptyList()
+            error("Failed to update index of available distributions for PyPI repository ${repository.name}: ${repository.url}")
         }
     }
 
     suspend fun downloadMetadata(packageName: String, repository: PyPackagesRepository): JsonPackageMetadataInfo {
-        val response: HttpResponse = httpClient.use {
-            it.request("${repository.url}/pypi/$packageName/json")
+        val response: HttpResponse = try {
+            httpClient.use {
+                it.request("${repository.url}/pypi/$packageName/json")
+            }
+        } catch (exception: Throwable) {
+            error("Failed to download metadata for package '$packageName' from ${repository.name}: ${repository.url}")
         }
         return jsonParser.decodeFromString(response.readText())
     }
