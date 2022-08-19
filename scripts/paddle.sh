f036c13e6d116d636fe4c9c957e23a1770449310
Vladislav.Tankov <vdtankov@gmail.com>
2022-08-19 14:03:11 +0200

    Update scripts to support binary artifacts

--- scripts/paddle.sh
+++ scripts/paddle.sh
@@ -7,76 +7,118 @@ CHANGELOG_URL='https://github.com/tanvd/paddle/blob/master/CHANGELOG.md'
 PADDLE_HOME=$HOME/.local/share/paddle
 FIRST_ARG=$1
 
-check_prerequisites () {
-    local required_utils="java curl jq"
-
-    for util in ${required_utils}
-    do
-        which ${util} > /dev/null
-        if [[ $? -eq 1 ]]; then
-            echo "\"$util\" has to be installed to use the wrapper"
-            exit 1
-        fi
-    done
+get_bin_type() {
+  local return=''
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    return='linux'
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    return='mac'
+  elif [[ "$OSTYPE" == "cygwin" ]]; then
+    return='linux'
+  else
+    return='jar'
+  fi
+  echo "$return"
 }
 
-check_config () {
-    [[ ! -d ${PADDLE_HOME} ]] && mkdir -p ${PADDLE_HOME}
-    [[ ! -f ${PADDLE_HOME}/version ]] && touch ${PADDLE_HOME}/version
+check_prerequisites() {
+  local required_utils=$1
+
+  for util in ${required_utils}; do
+    which "${util}" >/dev/null
+    if [[ $? -eq 1 ]]; then
+      echo "\"$util\" has to be installed to use the wrapper"
+      exit 1
+    fi
+  done
 }
 
-check_for_updates () {
-    local cur_ver=$(cat ${PADDLE_HOME}/version)
-    local latest_ver=$(echo ${LATEST_BUILD_INFO} | jq -r '.name')
-    local jar_name=${PADDLE_HOME}/${APP_NAME}.jar
+check_config() {
+  [[ ! -d ${PADDLE_HOME} ]] && mkdir -p "${PADDLE_HOME}"
+  [[ ! -f ${PADDLE_HOME}/version ]] && touch "${PADDLE_HOME}/version"
+}
 
-    if [[ ${latest_ver} == "" || ${latest_ver} == "null" ]]; then
-        echo "WARNING: Couldn't connect to GitHub to check for updates."
-        return
+check_for_updates() {
+  local cur_ver
+  cur_ver=$(cat "${PADDLE_HOME}"/version)
+
+  local latest_ver
+  latest_ver=$(echo "${LATEST_BUILD_INFO}" | jq -r '.name')
+
+  local bin_type
+  bin_type=$(get_bin_type)
+
+  local bin_name
+  if [[ "$bin_type" == "linux" || "$bin_type" == "mac" ]]; then
+    bin_name=${PADDLE_HOME}/${APP_NAME}
+  else
+    bin_name="${PADDLE_HOME}/${APP_NAME}.jar"
+  fi
+
+  if [[ ${latest_ver} == "" || ${latest_ver} == "null" ]]; then
+    echo "WARNING: Couldn't connect to GitHub to check for updates."
+    return
+  fi
+
+  if [[ ${cur_ver} == "" || ${cur_ver} != "${latest_ver}" || ! -f ${bin_name} || (${FIRST_ARG} == "check-updates" && ${cur_ver} != "${latest_ver}") ]]; then
+    if [[ ${latest_ver} == $(cat "${PADDLE_HOME}/skip-update" 2>/dev/null) && ${FIRST_ARG} != "check-updates" ]]; then
+      echo "You are using ${APP_NAME} ${cur_ver}, newer version ($(cat "${PADDLE_HOME}/skip-update")) is available but was skipped."
+      returnf
     fi
 
-    if [[ ${cur_ver} == "" || ${cur_ver} != ${latest_ver} || ! -f ${jar_name} || (${FIRST_ARG} == "check-updates" && ${cur_ver} != ${latest_ver}) ]]; then
-        if [[ ${latest_ver} == $(cat ${PADDLE_HOME}/skip-update 2> /dev/null) && ${FIRST_ARG} != "check-updates" ]]
-        then
-            echo "You are using ${APP_NAME} ${cur_ver}, newer version ($(cat ${PADDLE_HOME}/skip-update)) is available but was skipped."
-            return
+    echo "New version of ${APP_NAME} is available - ${latest_ver}"
+    echo "CHANGELOG: $CHANGELOG_URL"
+    echo "Would you like to update? (yes/no):"
+
+    read -r confirmation
+    if [[ ${confirmation} == "yes" ]]; then
+      echo "Updating..."
+      local url_to_use
+      local urls=( $(echo "${LATEST_BUILD_INFO}" | jq -r '.assets[].browser_download_url') )
+      for el in "${urls[@]}"; do
+        if [[ "$el" =~ .*${bin_type} ]]; then
+          url_to_use=$el
         fi
+      done
+      curl "$url_to_use" -L -s -o "${bin_name}" && echo "${latest_ver}" > "${PADDLE_HOME}/version"
 
-        echo "New version of ${APP_NAME} is available - ${latest_ver}"
-        echo "CHANGELOG: $CHANGELOG_URL"
-        echo "Would you like to update? (yes/no):"
+      if [[ $bin_type == "mac" || $bin_type == "linux" ]]; then
+        chmod +x "${PADDLE_HOME}/${APP_NAME}"
+      fi
 
-        read confirmation
-        if [[ ${confirmation} == "yes" ]]; then
-            echo "Updating..."
-            curl $(echo ${LATEST_BUILD_INFO} | jq -r '.assets[].browser_download_url') -L -s -o ${jar_name} && echo ${latest_ver} > ${PADDLE_HOME}/version
-            echo "Updated!"
-        else
-            echo "${latest_ver}" > ${PADDLE_HOME}/skip-update
-
-            echo "Update to ${latest_ver} skipped."
-            echo "You can always check for updates by running \"${APP_NAME} check-updates\""
-        fi
+      echo "Updated!"
     else
-        echo "You are using latest version of ${APP_NAME} - ${cur_ver}"
-    fi
+      echo "${latest_ver}" >"${PADDLE_HOME}/skip-update"
 
-    if [[ ${FIRST_ARG} == "check-updates" ]]
-    then
-        echo "Exiting..."
-        exit 0
+      echo "Update to ${latest_ver} skipped."
+      echo "You can always check for updates by running \"${APP_NAME} check-updates\""
     fi
+  else
+    echo "You are using latest version of ${APP_NAME} - ${cur_ver}"
+  fi
+
+  if [[ ${FIRST_ARG} == "check-updates" ]]; then
+    echo "Exiting..."
+    exit 0
+  fi
 }
 
-execute () {
-    java -jar ${PADDLE_HOME}/${APP_NAME}.jar "$@"
+execute() {
+  local bin_type
+  bin_type=$(get_bin_type)
+  if [[ "$bin_type" == "linux" || "$bin_type" == "mac" ]]; then
+    "${PADDLE_HOME}/${APP_NAME}" "$@"
+  else
+    check_prerequisites "java"
+    java -jar "${PADDLE_HOME}/${APP_NAME}.jar" "$@"
+  fi
 }
 
-main () {
-    check_prerequisites
-    check_config
-    check_for_updates
-    execute "$@"
+main() {
+  check_prerequisites "jq curl"
+  check_config
+  check_for_updates
+  execute "$@"
 }
 
 main "$@"
