a3aab6eea9466a298cdccf7e54900b01b1f87c6b
Mark Vavilov <vavilovmark@gmail.com>
2022-12-09 17:26:35 +0400

    change python script working directory from rootDir to moduleDir

--- idea/src/main/kotlin/io/paddle/idea/execution/state/PythonScriptCommandLineStateProvider.kt
+++ idea/src/main/kotlin/io/paddle/idea/execution/state/PythonScriptCommandLineStateProvider.kt
@@ -21,7 +21,7 @@ class PythonScriptCommandLineStateProvider : PaddleTaskRunProfileStateProvider<R
             scriptParameters = task.arguments.joinToString(" ")
             sdkHome = module.pythonSdk?.homePath
             isModuleMode = task.isModuleMode
-            workingDirectory = context.rootDir.absolutePath
+            workingDirectory = context.moduleDir.absolutePath
         }
 
         updateBeforeRunTasks(task, context)
