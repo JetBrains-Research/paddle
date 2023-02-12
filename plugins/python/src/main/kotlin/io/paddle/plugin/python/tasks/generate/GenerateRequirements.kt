412fae0e2933738b02e42de55a5e9f4a8a5f390d
khbminus <khbminus@gmail.com>
2023-02-12 15:43:44 +0100

    Fixed comments in PR

--- plugins/python/src/main/kotlin/io/paddle/plugin/python/tasks/generate/GenerateRequirements.kt
+++ plugins/python/src/main/kotlin/io/paddle/plugin/python/tasks/generate/GenerateRequirements.kt
@@ -1,38 +1,53 @@
 package io.paddle.plugin.python.tasks.generate
 
-import io.paddle.plugin.python.extensions.repositories
-import io.paddle.plugin.python.extensions.requirements
+import io.paddle.plugin.python.dependencies.repositories.PyPackageRepository
+import io.paddle.plugin.python.extensions.*
 import io.paddle.plugin.python.tasks.PythonPluginTaskGroups
 import io.paddle.project.PaddleProject
 import io.paddle.tasks.Task
 import io.paddle.tasks.incremental.IncrementalTask
+import io.paddle.utils.hash.Hashable
+import io.paddle.utils.hash.hashable
 import java.io.File
 
 class GenerateRequirements(project: PaddleProject) : IncrementalTask(project) {
     override val group: String = PythonPluginTaskGroups.GENERATE
     override val id: String = "requirements"
+    override val inputs: List<Hashable>
+        get() = listOf(project.requirements.descriptors.hashable(), project.requirements.resolved.map { it.toString().hashable() }.toList().hashable(),
+            project.repositories.descriptors.hashable())
+    override val outputs: List<Hashable>
+        get() = listOf(getRequirementsText().hashable())
     override val dependencies: List<Task>
         get() = listOf(
             project.tasks.getOrFail("resolveRepositories"),
             project.tasks.getOrFail("resolveInterpreter")
         ) + project.subprojects.getAllTasksById(this.id)
 
-
-    override fun act() {
+    private fun getRequirementsText(): String {
         // TODO: add support of find-links
         val notResolved = project.requirements.descriptors
         val resolved = project.requirements.resolved
-        val requirementsFile = File(REQUIREMENTS_FILE).also { it.writeText("") }
 
         // FIXME: local/packages with direct link are not printed correctly
-        notResolved.groupBy { descriptor -> resolved.find { it.name == descriptor.name }?.repo }.forEach { (repo, pkgs) ->
-            if (repo != null && repo != project.repositories.resolved.primarySource) {
-                requirementsFile.appendText("--extra-index-url ${repo.url}\n")
+        return buildString {
+            if (project.repositories.resolved.primarySource != PyPackageRepository.PYPI_REPOSITORY) {
+                appendLine("--index-url ${project.repositories.resolved.primarySource.url}")
             }
-            pkgs.forEach {
-                requirementsFile.appendText("${it.name}${it.versionSpecifier?.clauses?.joinToString(separator = ", ", prefix = " ") ?: ""}\n")
+            notResolved.groupBy { descriptor -> resolved.find { it.name == descriptor.name }?.repo }.forEach { (repo, pkgs) ->
+                if (repo != null && repo != project.repositories.resolved.primarySource) {
+                    appendLine("--extra-index-url ${repo.url}")
+                }
+                pkgs.forEach {
+                    appendLine("${it.name}${it.versionSpecifier?.clauses?.joinToString(separator = ", ", prefix = " ") ?: ""}")
+                }
             }
         }
+    }
+
+    override fun act() {
+        File(project.workDir, REQUIREMENTS_FILE).writeText(getRequirementsText())
+
     }
 
     companion object {
