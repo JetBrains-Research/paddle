98c9da4558a0f95a16a17ed1d038554c48d00aaf
khbminus <khbminus@gmail.com>
2023-03-15 19:13:07 +0100

    Added example run configuration

--- example/src/main/main.py
+++ example/src/main/main.py
@@ -5,7 +5,7 @@ from bugloc.app import Bar
 if __name__ == '__main__':
     print(sys.path)
     print(sys.argv)
-    print(os.getenv('CUSTOM_ENV'))
+    print(os.getenv('CUSTOM_ENV')) # You can find an example run configuration in .run folder
     for i in range(100000000):
         pass
     print("Hello world!")
